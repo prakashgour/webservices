49994764c2565794f18e99054a77ca1ee9b469ee
prakashgour <pgour.com@gmail.com>
2017-05-20 02:47:05 +0530

    @DataJpaTest added

--- ShopsAPI_gradle/src/test/java/com/prakash/shopapi/io/ShopsApiGradleApplicationTests.java
+++ ShopsAPI_gradle/src/test/java/com/prakash/shopapi/io/ShopsApiGradleApplicationTests.java
@@ -1,12 +1,16 @@
 package com.prakash.shopapi.io;
 
 import org.junit.Test;
+
+
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class ShopsApiGradleApplicationTests {
 	
 	
@@ -15,8 +19,6 @@ public class ShopsApiGradleApplicationTests {
 	public void contextLoads() {
 		
 		
-	}
-
-	
+	}	
 	
 }
