ddbf13ac02a9859b9e7eb2f65a3ba9e2737b2189
prakashgour <pgour.com@gmail.com>
2017-05-25 13:56:34 +0530

    Comments added

--- ShopsAPI_gradle/src/test/java/com/prakash/shopapi/io/ShopsApiGradleApplicationTests.java
+++ ShopsAPI_gradle/src/test/java/com/prakash/shopapi/io/ShopsApiGradleApplicationTests.java
@@ -8,11 +8,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * This is the test class to execute test cases
+ * 
+ * @author Prakash Gour
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DataJpaTest
 public class ShopsApiGradleApplicationTests {
-	
 	
 	
 	@Test
