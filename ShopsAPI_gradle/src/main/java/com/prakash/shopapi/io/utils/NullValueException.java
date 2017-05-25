759b18deda08d7da7ab6a87e0adf2cec558d022c
prakashgour <pgour.com@gmail.com>
2017-05-26 00:42:37 +0530

    Update NullValueException.java

--- ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/utils/NullValueException.java
+++ ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/utils/NullValueException.java
@@ -1,4 +1,4 @@
-package com.prakash.shopapi.io.exception;
+package com.prakash.shopapi.io.utils;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
