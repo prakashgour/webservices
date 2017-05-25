db910ac42ecf1041be0b5105b9b922ca8de091de
prakashgour <pgour.com@gmail.com>
2017-05-25 13:43:27 +0530

    Comments added

--- ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/repository/ShopRepository.java
+++ ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/repository/ShopRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.prakash.shopapi.io.model.Shop;
 
+/**
+ * This is the CRUD repository to manage the shop details in database.
+ * 
+ * @author Prakash Gour
+ * @see {@link org.springframework.data.repository.CrudRepository}}
+ */
 public interface ShopRepository extends CrudRepository<Shop, Long> {
 	public Shop findByShopName(String shopName);
 }
