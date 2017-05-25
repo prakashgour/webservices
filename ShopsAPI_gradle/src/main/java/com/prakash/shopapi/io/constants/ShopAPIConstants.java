1ab952f6fe8d13c7f4b718f9e4f6875a6e314c5d
prakashgour <pgour.com@gmail.com>
2017-05-25 13:39:55 +0530

    Code change

--- ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/constants/ShopAPIConstants.java
+++ ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/constants/ShopAPIConstants.java
@@ -1,5 +1,23 @@
 package com.prakash.shopapi.io.constants;
 
+/**
+ * This class <code> ShopAPIConstants.java</code> provides the constants used in
+ * the ShopApi.
+ * 
+ * @author Prakash Gour  occurred
+ */
 public class ShopAPIConstants {
 	public final static String API_KEY = "AIzaSyDQh7W0NPCP4R2-hRwysXtwJbQiWFgBLwg";
+	public final static String SHOP_EXIST_MSG = "Shop with same details is already exist in DB where ";
+	public final static String INCORRECT_ADD="Please enter correct address incorrec";
+	public static final String NULL_VALUE_NOT_ALLOW = "All fields are mandatory.";
+	public static final String NO_SHOP_AVAIL="No shop availabe at the latlong: " ;
+	public static final String SHOP_CREATED = "Shop is Added successfully";
+	public static final String NEAREST_FOUND = "Nearest shop is found";
+	public static final String RETURN_SHOPS = "Returning List of all shops from store ";
+	public static final String SHOP_UPDATED = "Shop updated successfully";
+	public static final String DELETED = "Shop has been deleted with shop id :";
+	public static final String MANDATORY = " ERROR OCCURRED :All fields are mandatory";
+	public static final String NEAREST_NOT_FOUND = "No Shop found";
+	public static final String EMPTY_STORE = "No shop available in database.";
 }
