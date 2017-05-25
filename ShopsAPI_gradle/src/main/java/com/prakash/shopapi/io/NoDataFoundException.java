40c92870a8c16ecc61f27675a3d509fc9a2e48ed
prakashgour <pgour.com@gmail.com>
2017-05-25 13:34:07 +0530

    Custom exception

---
+++ ShopsAPI_gradle/src/main/java/com/prakash/shopapi/io/NoDataFoundException.java
@@ -0,0 +1,25 @@
+package com.prakash.shopapi.io.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NoDataFoundException extends RuntimeException {
+	private static final long serialVersionUID = -3866842046845921983L;
+	
+	public NoDataFoundException() {
+		super();
+	}
+
+	public NoDataFoundException(String errorMsg) {
+		super(errorMsg);
+	}
+	
+	public NoDataFoundException(Throwable cause) {
+		super(cause);
+	}
+	
+	public NoDataFoundException(String errorMsg, Throwable cause) {
+		super(errorMsg, cause);
+	}
+}
